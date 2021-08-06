a484f2053a811e8a1191b3d3d9992314a0a46285
HAKAN TURINAY <hakan@qreal.io>
2021-08-06 18:40:29 +0300

    index

--- script.js
+++ script.js
@@ -141,7 +141,7 @@ function main() {
     var testBool = false
   
     animate()
-    if (Math.abs(vector.x)> 0 && Math.abs(vector.x) <0.01 && testBool == false){
+    if (Math.abs(vector.x)> 0 && Math.abs(vector.x) <0.003 && testBool == false){
       new TWEEN.Tween( text.material ).to( { opacity: 1 }, 100 ).start();
       // console.log("tween started")
       testBool = true;
