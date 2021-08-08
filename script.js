d6e91794790933e14290133dbb1d96fc821ce171
HAKAN TURINAY <hakan@qreal.io>
2021-08-09 00:30:17 +0300

    index

--- script.js
+++ script.js
@@ -158,15 +158,16 @@ scene5.add( cube )
     var vector = camera.position.clone();
     // console.log(vector.x)
     var testBool = false
-    if (vector.x > 0.0005 && vector.x <0.013 && testBool == false){
+    if (vector.x > 0.0005 ){
       new TWEEN.Tween( text.material ).to( { opacity: 1 }, 100 ).start();
       animate()
       // console.log("tween started")
       testBool = true;
-    } if(vector.x >0.013 || vector.x <0){
-      new TWEEN.Tween( text.material ).to( { opacity: 0 }, 100 ).start();
-      testBool = false;
-    }
+    } 
+    // if(vector.x >0.013 || vector.x <0){
+    //   new TWEEN.Tween( text.material ).to( { opacity: 0 }, 100 ).start();
+    //   testBool = false;
+    // }
 
 
 
